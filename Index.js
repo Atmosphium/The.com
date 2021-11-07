bb2ccc1fd3c5c4ae3a7a8cec5e98c7a4919c6bbc
Atmospherium <80439124+Atmosphium@users.noreply.github.com>
2021-11-07 13:42:55 +0400

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,3 @@
+require ('mongoose').connect ('mongodb+srv://Database:Datebase@cluster0.vc0pg.mongodb.net/sampleDatabase?retryWrites=true&w=majority', { keepAlive: true, useNewUrlParser: true, useUnifiedTopology: true })
+
+require ('./Configurations/Express')
